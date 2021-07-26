d4da9b1789f9f69d86a3c5c5037df8b61fc8ecbe
Riki420 <girottiriccardo1998@gmail.com>
2021-07-26 17:45:13 +0200

    giro nell'array per stampare le carte

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+//*  GIRO NELL'ARRAY DI OGGETTI PER CREARE IL TEMPLATE
+//creo una stringa vuota
+let iconsTemplate = '';
+// giro nell'array per crearne il template
+icons.forEach((icon) =>{
+    iconsTemplate += `
+    <div class="col col-sm-4 col-md-2 text-center">
+        <div class="card">
+            <div class="card-body">
+                <i class="${icon.family} ${icon.prefix}${icon.name} fa-x2"></i>
+                <h6>${icon.name.toUpperCase()}</h6>
+            </div>
+        </div>
+    </div>
+    `
+});
+
+const cardSection = document.querySelector('#icons .row');
+cardSection.innerHTML = iconsTemplate;
