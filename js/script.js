d1414221ab5c8831853e0f2c0af09c28b68a966b
Riki420 <girottiriccardo1998@gmail.com>
2021-07-26 17:55:10 +0200

    aggiunto l'offset

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@ const renderIcons = (icons, targetElement) => {
    //*  GIRO NELL'ARRAY DI OGGETTI PER CREARE IL TEMPLATE
 //creo una stringa vuota
 let iconsTemplate = '';
+
 // giro nell'array per crearne il template
-icons.forEach((icon) =>{
+icons.forEach((icon, index) =>{
+    //creo una stringa vuota
+    let hasOffSet = '';
+    if(index == 0 || index % 5 == 0){       //quando l'index è = a 0 aggiunge un offset
+    hasOffSet = 'offset-md-1';
+    }
     iconsTemplate += `
-    <div class="col col-sm-4 col-md-2 text-center">
+    <div class="col col-sm-4 col-md-2 text-center ${hasOffSet}">
         <div class="card">
             <div class="card-body">
                 <i class="${icon.family} ${icon.prefix}${icon.name} fa-x2"></i>
@@ -20,9 +26,6 @@ targetElement.innerHTML = iconsTemplate;
 
 }
 
-
-
-
-
+//*STAMPO IN PAGINA
 const cardSection = document.querySelector('#icons .row');
 renderIcons(icons, cardSection);
